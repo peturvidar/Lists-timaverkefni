0a08077d4068004d90932d95edacc79b32534064
Hjortur Palmi Palsson <hjorri@Hjorturs-MacBook-Pro.local>
2021-09-19 20:15:37 +0200

    init

---
+++ bin/Test33.dart
@@ -0,0 +1,5 @@
+import 'package:Test33/Test33.dart' as Test33;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Test33.calculate()}!');
+}
---
+++ test/Test33_test.dart
@@ -0,0 +1,8 @@
+import 'package:Test33/Test33.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
